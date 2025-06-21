abfb15092492f24a7d20e46ece221d84174b199f
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-06-21 18:24:53 +0530

    intial vite & tailwind setup with empty header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+            <div>Logo</div>
+            <div></div>
+        </>
+    )
+}
+
+export default Header;

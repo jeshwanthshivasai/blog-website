ea8ed249d36af1a1186319a37d9354aac1d4c6d2
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-06-21 22:58:46 +0530

    basic header with styling done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React from "react"
 import Header from "./components/Header"
 
-function App() {
-
+const App = () =>  {
   return (
     <>
+    
       <Header/>
     </>
   )
 }
 
-export default App
+export default App;

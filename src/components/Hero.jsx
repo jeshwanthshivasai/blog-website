9c9e38e654fc2baceda635082d71d3708063b9c4
Nishi Surti <01nishisurti@gmail.com>
2025-06-26 10:22:29 -0400

    Fixed hero section image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,40 @@
 const Hero = () => {
     return (
         <>
-            <main className="bg-neutral-800 flex justify-between text-white ">
-                <div>
-                    <h1>Text</h1>
-           
+            <main className="bg-neutral-800 flex h-screen">
+                <div className="flex flex-col w-1/2 justify-center z-20 ml-12 ">
+                    <h1 className=" text-white text-2xl font-normal tracking-widest mb-6">ORESTIS GEORGIOU</h1>
+                   
+                     <div className="bg-orange-400 h-1 "></div>
+                      <h1 className="text-8xl font-bold text-white leading-tight text-right">
+                            RESEARCHER.
+                            <br />
+                            INVENTOR.
+                            <br />
+                            AUTHOR.
+                         </h1>
+
                 </div>
-                <img src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/c404d93c-aa04-4838-ab6d-2af4a3b8c153/orestis-georgiou.jpg" className="w-2xl" />
+                   
+
+                <div className="w-1/2 z-10 absolute right-0">
+                <img src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/c404d93c-aa04-4838-ab6d-2af4a3b8c153/orestis-georgiou.jpg" className="object-cover w-full " />
+                </div>
+          
             </main>
         </>
     )
 }
 
 export default Hero
+
+/* 
+DO NOT USE CHAT GPT FINAL WARNING  */
+
+
+
+
+
 
 
 // const Hero = () => {

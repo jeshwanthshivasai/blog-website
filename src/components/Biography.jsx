578a1bc598eb1170f7b1c3e60e4128c3de14371a
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-06-27 13:01:17 +0530

    Hero perfection and basic styling and skeleton in bio

--- src/components/Biography.jsx
+++ src/components/Biography.jsx
@@ -1,9 +1,17 @@
 const bio = () => {
     return (
         <>
-            <div>
-                
+            <div className="flex justify-evenly">
+                <div className="">
+                    <img src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/466df5e6-c3c4-469d-981e-5183928c5a0d/orestis-georgiou-biography.jpg" />
+                </div>
+                <div className="bg-stone-100">
+                    <h1 className="text-5xl text-orange-500 font-extrabold">BIOGRAPHY</h1>
+                    <p className="text-3xl font-extralight">Dr Orestis Georgiou is an author of 6 patents, 2 books, and 100+ academic papers that have been published in leading journals and conferences of Mathematics, Physics, Computer Science, Engineering and Medicine. He has also been co-awarded R&D grants in excess of €10 million, including a Marie Curie Individual Fellowship, and is the recipient of the prestigious 2019 IEEE Heinrich Hertz award. Finally, as Head of R&D Partnerships at Ultraleap, he is passionate about haptic technologies, networks, and spatial XR computing and oversees all our external R&D collaboration activities and grant-funded projects.</p>
+                </div>
             </div>
         </>
     )
 }
+
+export default bio
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,10 @@ const Hero = () => {
             <main className="bg-neutral-800 flex text-white">
                 <div className="relative inline-block z-1">
                     <div>
-                        <p className="mt-35 ml-20 text-3xl underline decoration-orange-500 decoration-[5px] underline-offset-45 z-10">ORESTIS GEORGIOU</p>
+                        <p className="mt-35 ml-20 text-3xl">ORESTIS GEORGIOU</p>
+                        <div className="h-2 w-6xl mt-10 ml-20 bg-orange-500"></div>
                     </div>
-                    <div className="flex justify-end text-9xl mt-20 mr-40">
+                    <div className="flex justify-end text-9xl mt-10 mr-40">
                         <ul className="space-y-5 text-right font-extrabold">
                             <li>RESEARCHER.</li>
                             <li>INVENTOR.</li>
@@ -14,11 +15,11 @@ const Hero = () => {
                         </ul>
                     </div>
                     <div>
-                        <p className="ml-20 mt-60 text-3xl text-left z-10 w-7xl pb-50"><span className="text-5xl font-bold mb-50">Hello there,</span><br/><br/>I am Hari Prasad from Telangana state, India. Completed my graduation from IIT Bombay in Electrical engineering in 2018. Currently working as R&D Engineer at Daikin Industries Ltd in Osaka, Japan.</p>
+                        <p className="ml-20 mt-60 text-3xl font-extralight text-left z-10 w-7xl pb-50"><span className="text-5xl text-orange-500 font-extrabold mb-50">Hello there,</span><br/><br/>I am Hari Prasad from Telangana state, India. Completed my graduation from IIT Bombay in Electrical engineering in 2018. Currently working as R&D Engineer at Daikin Industries Ltd in Osaka, Japan.</p>
                     </div>
                 </div>
-                <div className="absolute -z-0  ml-230">
-                    <img src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/c404d93c-aa04-4838-ab6d-2af4a3b8c153/orestis-georgiou.jpg" className="h-220" />
+                <div className="absolute -z-0  ml-230 h-220">
+                    <img src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/c404d93c-aa04-4838-ab6d-2af4a3b8c153/orestis-georgiou.jpg"/>
                 </div>
             </main>
         </>

f2f5878bb9f1afac0ae6045890c25634c2490ac0
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-07-01 22:35:10 +0530

    footer final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,21 +23,33 @@
 const Footer = () => {
     return (
         <>
-            <footer className="bg-neutral-800 text-white py-10 px-20">
+            <footer className="bg-neutral-800 text-white py-10">
                 <div className="flex flex-col justify-between items-center">
-                    {/* Navigation Links */}
-                    <ul className="space-y-5 text-lg font-light">
-                        <li className="hover:text-orange-500 cursor-pointer">Home</li>
-                        <li className="hover:text-orange-500 cursor-pointer">About</li>
-                        {/* <li className="hover:text-orange-500 cursor-pointer">Publications</li> */}
-                        <li className="hover:text-orange-500 cursor-pointer">Blog</li>
-                        <li className="hover:text-orange-500 cursor-pointer">Contact</li>
-                    </ul>
-
-                    {/* Copyright */}
-                    <p className="mt-10 text-sm text-gray-400 text-center">
-                        &copy; {new Date().getFullYear()} Pothuraju Hari Prasad. All rights reserved.
-                    </p>
+                    <div className="flex space-x-20 mt-5">
+                        <ul className="flex space-x-10 text-lg font-light">
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">Home</li></a>
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">About</li></a>
+                            {/* <a><li className="hover:text-orange-500 cursor-pointer">Publications</li></a> */}
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">Blog</li></a>
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">Contact</li></a>
+                        </ul>
+                        {/* <ul className="flex space-x-10 text-lg font-light">
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">Home</li></a>
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">About</li></a>
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">Blog</li></a>
+                            <a><li className="hover:text-orange-500 hover:underline hover:underline-offset-4 cursor-pointer">Contact</li></a>
+                        </ul> */}
+                    </div>
+                    <div>
+                        <p className="text-lg text-orange-500 text-center mb-5 mt-10">
+                            &copy; {new Date().getFullYear()} <span className="text-white font-bold">Pothuraju Hari Prasad.</span> All rights reserved.
+                        </p>
+                        <p className="text-sm text-orange-500 text-center mt-10 mb-5">| Designed & Developed | 
+                            <span className="text-white">
+                            <a href="https://jess-web-dev-portfolio.vercel.app/"> Jeshwanth Shiva Sai</a>
+                            </span> |
+                        </p>
+                    </div>
                 </div>
             </footer>
         </>

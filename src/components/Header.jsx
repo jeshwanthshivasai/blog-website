c55b36f540f68d21f71ccd3bc3e3c02fd26ea73d
Jeshwanth Shiva Sai <havingthesensesofaspider@gmail.com>
2025-07-02 17:18:09 +0530

    accessibility and responsivness in header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,50 @@
 const Header = () => {
     return (
         <>
-            <nav className="bg-neutral-800 flex justify-between px-6 py-6">
-                <img src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/1550839443584-XL6BMJZ93MCMRQX2BPGP/Orestis-Georgiou-logo-web-white.png" className="h-20 w-auto ml-10" />
-                <ul className="flex space-x-30 items-center text-orange-500 mr-20 ">
-                    <li><a href="#home" className="hover:text-white hover:underline-offset-2 decoration-2 decoration-white uppercase text-lg">Home</a></li>
-                    <li><a href="#about" className="hover:text-white hover:underline-offset-2 decoration-2 decoration-white uppercase text-lg">About</a></li>
-                    <li><a href="#blog" className="hover:text-white hover:underline-offset-2 decoration-2 decoration-white uppercase text-lg">Blog</a></li>
-                    <li><a href="#contact" className="hover:text-white hover:underline-offset-2 decoration-2 decoration-white uppercase text-lg">Contact</a></li>
+            <nav 
+                className="bg-neutral-800 flex justify-between items-center px-6 py-6"
+                role="navigation"
+                aria-label="Main Navigation"
+            >
+                <a href="#home" className="ml-10" aria-label="Go to Homepage">
+                    <img 
+                        src="https://images.squarespace-cdn.com/content/v1/5c6fd1c7c2ff616d47ed3809/1550839443584-XL6BMJZ93MCMRQX2BPGP/Orestis-Georgiou-logo-web-white.png" 
+                        className="h-20 w-auto" 
+                    />
+                </a>
+                <ul className="flex space-x-30 items-center text-orange-500 mr-20 text-lg">
+                    <li>
+                        <a 
+                        href="#home" 
+                        className="hover:text-white focus:text-white hover:underline hover:underline-offset-4 uppercase"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                        href="#about"
+                        className="hover:text-white focus:text-white hover:underline hover:underline-offset-4 uppercase"
+                        >
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                        href="#blog" 
+                        className="hover:text-white focus:text-white hover:underline hover:underline-offset-4 uppercase"
+                        >
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                        href="#contact" 
+                        className="hover:text-white focus:text-white hover:underline hover:underline-offset-4 uppercase"
+                        >
+                            Contact
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </>
